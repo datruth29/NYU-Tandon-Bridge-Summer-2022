29adc215806a89c81e571214ed65d15764ebe288
Adam Collado <adam.collado@gmail.com>
2022-07-23 00:07:19 -0400

    Finished week 3 homework 4 cpp

---
+++ week3/homework/cpp/ac10281_hw3_q4.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+const int FLOOR_ROUND = 1;
+const int CEILING_ROUND = 2;
+const int ROUND = 3;
+
+int main()
+{
+    float real_number;
+    int rounding_method;
+
+    cout << "Please enter a Real number:" << endl;
+    cin >> real_number;
+
+    cout << "Choose your rounding method:" << endl
+    << "1. Floor round" << endl
+    << "2. Ceiling round" << endl
+    << "3. Round to the nearest whole number" << endl;
+    cin >> rounding_method;
+
+    switch(rounding_method)
+    {
+        case(FLOOR_ROUND):
+            cout << (int) real_number;
+        break;
+
+        case(CEILING_ROUND):
+            cout << (int) (real_number+1);
+        break;
+
+        case(ROUND):
+            cout << (int) (real_number+0.5);
+        break;
+
+        default:
+        cout << "You selected an invalid option";
+    }
+
+    return 0;
+}
