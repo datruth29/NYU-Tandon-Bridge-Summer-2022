3a3b6fdeff6807cfd7e7d5c158ebd6a43cc20abd
Adam Collado <adam.collado@gmail.com>
2022-07-23 00:21:50 -0400

    Finished week 3 homework 5 cpp

---
+++ week3/homework/cpp/ac10281_hw3_q5.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+const float POUNDS_TO_KILOS = 0.45359237;
+const float INCHES_TO_METERS = 0.0254;
+
+int main()
+{
+    float weight, height;
+
+    cout << "Please enter weight (in pounds): ";
+    cin >> weight;
+
+    cout << "Please enter height (in inches): ";
+    cin >> height;
+
+    // Convert to kilograms
+    weight *= POUNDS_TO_KILOS;
+    
+    // Convert to meters;
+    height *= INCHES_TO_METERS;
+
+    float bmi = weight/(height*height);
+    string status;
+
+    if (bmi < 18.5)
+    {
+        status = "Underweight";
+    }
+
+    if (bmi >= 18.5 && bmi < 25)
+    {
+        status = "Normal";
+    }
+    if (bmi >= 25 && bmi < 30)
+    {
+        status = "Overweight";
+    }
+    if (bmi >= 30)
+    {
+        status = "Obese";
+    }
+
+    cout << "The weight status is: " << status;
+
+    return 0;
+}
