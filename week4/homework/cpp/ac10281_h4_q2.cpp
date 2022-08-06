552af7eca52ef4bfe4d1c14ace6701be742b012c
Adam Collado <adam.collado@gmail.com>
2022-08-05 22:13:01 -0400

    Updated homeworks for week 4

--- week4/homework/cpp/ac10281_h4_q2.cpp
+++ week4/homework/cpp/ac10281_h4_q2.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+const char ROMAN_SYMBOL_M = 'M';
+const char ROMAN_SYMBOL_D = 'D';
+const char ROMAN_SYMBOL_C = 'C';
+const char ROMAN_SYMBOL_L = 'L';
+const char ROMAN_SYMBOL_X = 'X';
+const char ROMAN_SYMBOL_V = 'V';
+const char ROMAN_SYMBOL_I = 'I';
+
+const int ROMAN_VALUE_M = 1000;
+const int ROMAN_VALUE_D = 500;
+const int ROMAN_VALUE_C = 100;
+const int ROMAN_VALUE_L = 50;
+const int ROMAN_VALUE_X = 10;
+const int ROMAN_VALUE_V = 5;
+const int ROMAN_VALUE_I = 1;
+
 int main()
 {
     int userInput;
@@ -17,38 +33,38 @@ int main()
     {
         if (userInput >= 1000)
         {
-            romanValue += "M";
-            userInput -= 1000;
+            romanValue += ROMAN_SYMBOL_M;
+            userInput -= ROMAN_VALUE_M;
         }
         else if (userInput >= 500)
         {
-            romanValue +="D";
-            userInput -= 500;
+            romanValue += ROMAN_SYMBOL_D;
+            userInput -= ROMAN_VALUE_D;
         }
         else if (userInput >= 100)
         {
-            romanValue += "C";
-            userInput -= 100;
+            romanValue += ROMAN_SYMBOL_C;
+            userInput -= ROMAN_VALUE_C;
         }
         else if (userInput >= 50)
         {
-            romanValue += "L";
-            userInput -= 50;
+            romanValue += ROMAN_SYMBOL_L;
+            userInput -= ROMAN_VALUE_L;
         }
         else if (userInput >= 10)
         {
-            romanValue += "X";
-            userInput -= 10;
+            romanValue += ROMAN_SYMBOL_X;
+            userInput -= ROMAN_VALUE_X;
         }
         else if (userInput >= 5)
         {
-            romanValue += "V";
-            userInput -= 5;
+            romanValue += ROMAN_SYMBOL_V;
+            userInput -= ROMAN_VALUE_V;
         }
         else
         {
-            romanValue += "I";
-            userInput -= 1;
+            romanValue += ROMAN_SYMBOL_I;
+            userInput -= ROMAN_VALUE_I;
         }
     }
 
