258ce1292acd308ee7f7dd009d23804a23f8f33c
Adam Collado <adam.collado@gmail.com>
2022-08-13 16:59:54 -0400

    Added exam 1 answers

---
+++ Exams/Exam 1/ac10281_q1to11.cpp
@@ -0,0 +1,27 @@
+/*
+* Name: Adam Collado
+* NetId: ac10281
+* 1a) 13066
+* 1b) -15
+* 2) a
+* 3) b
+* 4) c
+* 5) b
+* 6) c
+* 7a) true
+* 7b) false
+* 7c) false
+* 7d) true
+* 7e) true
+* 7f) true
+* 7g) false
+* 7h) false
+* 7i) false
+* 7j) true
+* 7k) false
+* 7l) true
+* 8) c
+* 9) a
+* 10) c
+* 11) a and c are true
+*/
