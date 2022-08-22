73dbba9730bdee18d1ba6fb8082a8567733cc189
Adam Collado <adam.collado@gmail.com>
2022-08-22 01:12:15 -0400

    Starting week 7

---
+++ week7/homework/cpp/ac10281_hw6_q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int printMonthCalender(int numOfDays, int startingDay);
+void printYearCalendar(int year, int startingDay);
+
+int main()
+{
+
+    return 0;
+}
+
+int printMonthCalender(int numOfDays, int startingDay)
+{
+
+    return 0;
+}
+
+void printYearCalender(int year, int startingDay)
+{
+
+}
