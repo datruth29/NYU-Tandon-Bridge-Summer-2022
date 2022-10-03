79762fb2677b28b19743905d41af6ccb0d6621b5
Adam Collado <adam.collado@gmail.com>
2022-10-02 23:38:11 -0400

    Final Updates to Homework 11

--- week11/homework/cpp/ac10281_hw11_q3.cpp
+++ week11/homework/cpp/ac10281_hw11_q3.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int minInArray1(int arr[], int arrSize);
 int minInArray2(int arr[], int low, int high);
+int old_minInArray2(int arr[], int low, int high);
 
 int main() {
     int arr[10] = {9, -2, 14, 12, 3, 6, 2, 1, -9, 15};
     int res1, res2, res3, res4;
+    int arr[2] = {1, 0};
+    int res5, res6;
 
     res1 = minInArray1(arr, 10);
     res2 = minInArray2(arr, 0, 9);
@@ -41,10 +44,35 @@ int minInArray2(int arr[], int low, int high) {
         return arr[low];
     }
 
+    if (low - high == 1) {
+        if (arr[low] < arr[high])
+            return arr[low];
+        return arr[high];
+    }
+
+    int rec_result = minInArray2(arr, ++low, --high);
+    int current_min;
+
+    if (arr[low] < arr[high])
+        current_min = arr[low];
+    else
+        current_min = arr[high];
+    
+    if (rec_result < current_min)
+        return rec_result;
+    else
+        return current_min;
+    
+}
+
+int old_minInArray2(int arr[], int low, int high) {
+    if (low == high) {
+        return arr[low];
+    }
+
     if (arr[low] < arr[high]) {
         return minInArray2(arr, low, --high);
     } else {
         return minInArray2(arr, ++low, high);
     }
-
 }
