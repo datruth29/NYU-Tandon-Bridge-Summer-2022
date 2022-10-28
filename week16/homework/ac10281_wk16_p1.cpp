1c11dc3de1f529d6bda767664c2db97704ea5b97
Adam Collado <adam.collado@gmail.com>
2022-10-28 19:09:14 -0400

    Start of Homework for Week 16

---
+++ week16/homework/ac10281_wk16_p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+enum class Symbols
+{
+    OpenParan,
+    ClosedParan,
+    OpenCurly,
+    ClosedCurly,
+    OpenBracket,
+    ClosedBracket,
+    OpenProgram,
+    ClosedProgram
+};
+int main()
+{
+    int i = '{';
+    cout << i;
+
+}
---
+++ week16/homework/test.pas
@@ -0,0 +1,13 @@
+begin
+
+int main()
+{
+    int a, b;
+    int arr[];
+    arr[0] = {temp(1234)}
+    int b_arr[] = [{123}, {[(23[1])]}];
+
+    return 0;
+}
+
+end
