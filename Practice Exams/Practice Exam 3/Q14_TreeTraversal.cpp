f30dbff77645028711bc565625cb2139cd264af1
Adam Collado <datruth29@gmail.com>
2022-11-04 16:19:06 -0400

    Finished tree traversal Practice Exam 3

--- Practice Exams/Practice Exam 3/Q14_TreeTraversal.cpp
+++ Practice Exams/Practice Exam 3/Q14_TreeTraversal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 struct TreeNode
 {
@@ -8,17 +9,29 @@ struct TreeNode
     TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
 };
 bool isSameTree(TreeNode *p, TreeNode *q);
+bool isSymmetric(TreeNode* root);
+bool _isSymmetric(TreeNode* left, TreeNode* right);
+void levelOrder(TreeNode* root);
 
 int main()
 {
-    TreeNode h(10);
-    h.left = new TreeNode(30);
-    h.right = new TreeNode(20);
 
     TreeNode j(10);
-    j.left = new TreeNode(30);
+    j.left = new TreeNode(20);
+    j.right = new TreeNode(30);
 
-    std::cout << isSameTree(&j, &h);
+    TreeNode i(10);
+    i.left = new TreeNode(30);
+    i.right = new TreeNode(20);
+
+    TreeNode h(10);
+    h.left = &j;
+	h.right = &i;
+    
+
+    std::cout << isSameTree(&j, &h) << '\n';
+    std::cout << isSymmetric(&h) << '\n';
+    levelOrder(&h);
 
 }
 // Given two binary trees, write a function to check if they are the same or not.
@@ -67,10 +80,60 @@ bool isSymmetric(TreeNode *root)
     {
         return true;
     }
-    // TODO
+    if (root->left == nullptr || root->right == nullptr)
+    {
+        return false;
+    }
+
+    return _isSymmetric(root->left, root->right);
+
+}
+
+bool _isSymmetric(TreeNode* left, TreeNode* right)
+{
+    if (left == nullptr && right == nullptr)
+    {
+        return true;
+    }
+
+    if (left == nullptr || right == nullptr)
+    {
+        return false;
+    }
+
+    if (left->val != right->val)
+    {
+        return false;
+    }
+
+	return _isSymmetric(left->left, right->right)
+        && _isSymmetric(left->right, right->left);
+
 }
 // Given a binary tree, implement level order traversal
 void levelOrder(TreeNode *root)
 {
-    // TODO
+    std::queue<TreeNode*> tree_queue;
+    std::queue<TreeNode*> level_queue;
+
+    tree_queue.push(root);
+
+    while (!tree_queue.empty())
+    {
+        tree_queue.swap(level_queue);
+        while (!level_queue.empty())
+        {
+			TreeNode* node = level_queue.front();
+			level_queue.pop();
+
+            if (node->left != nullptr)
+                tree_queue.push(node->left);
+            if (node->right!= nullptr)
+                tree_queue.push(node->right);
+			
+			std::cout << node->val << ' ';
+        }
+        std::cout << '\n';
+
+    }
 }
