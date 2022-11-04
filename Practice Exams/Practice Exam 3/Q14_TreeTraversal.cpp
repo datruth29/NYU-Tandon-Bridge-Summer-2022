a30488dd260ab7de92e9b337632b9ff73e0d7ee4
Adam Collado <adam.collado@gmail.com>
2022-11-04 14:33:10 -0400

    Updated Q14 from Practice Exam 3

--- Practice Exams/Practice Exam 3/Q14_TreeTraversal.cpp
+++ Practice Exams/Practice Exam 3/Q14_TreeTraversal.cpp
@@ -7,17 +7,51 @@ struct TreeNode
     TreeNode *right;
     TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
 };
+bool isSameTree(TreeNode *p, TreeNode *q);
+
+int main()
+{
+    TreeNode h(10);
+    h.left = new TreeNode(30);
+    h.right = new TreeNode(20);
+
+    TreeNode j(10);
+    j.left = new TreeNode(30);
+
+    std::cout << isSameTree(&j, &h);
+
+}
 // Given two binary trees, write a function to check if they are the same or not.
 // Two binary trees are considered the same if they are structurally identical
 // and the nodes have the same value.
 bool isSameTree(TreeNode *p, TreeNode *q)
 {
-    bool value_check = (p->val == q->val);
+    if (p == nullptr && q == nullptr)
+    {
+        return true;
+    }
+    if (p == nullptr || q == nullptr)
+    {
+        return false;
+    }
     if (p->val != q->val)
     {
         return false;
     }
-    if (p->val)
+
+    bool left_side = isSameTree(p->left, q->left);
+    if (!left_side)
+    {
+        return false;
+    }
+
+    bool right_side = isSameTree(p->right, q->right);
+    if (!right_side)
+    {
+        return false;
+    }
+
+    return true;
 
     // TODO
 }
@@ -25,6 +59,14 @@ bool isSameTree(TreeNode *p, TreeNode *q)
 // (ie, symmetric around its center).
 bool isSymmetric(TreeNode *root)
 {
+    if (root == nullptr)
+    {
+        return true;
+    }
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        return true;
+    }
     // TODO
 }
 // Given a binary tree, implement level order traversal
